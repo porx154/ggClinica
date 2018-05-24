3ea830d8dbdc8b7c32449f9160af2e72895229ed
porx154 <37988377+porx154@users.noreply.github.com>
2018-05-23 23:54:02 -0500

    csmed

--- CLINICAMVC/CLINICAMVC/Models/Consultorio.cs
+++ CLINICAMVC/CLINICAMVC/Models/Consultorio.cs
@@ -8,6 +8,10 @@ namespace CLINICAMVC.Models
 {
     public class Consultorio
     {
-        
+        public int Id { get; set; }
+
+        [StringLength(5)]
+        public string DesConsultorio { get; set; }
+        public List<Medico> Medico { get; set; }
     }
 }
